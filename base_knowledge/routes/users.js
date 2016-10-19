32d53cbe954952de21b64a7bb0077407a5662d64
ding.luchao <116710782@qq.com>
2016-10-19 16:36:46 +0800

    add index.html

--- base_knowledge/routes/users.js
+++ base_knowledge/routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.sendFile('/tmp/angular/base_knowledge/views/index.html');
 });
 
 module.exports = router;
